8ead386af90fcddcb9dd85a5b873a60461c5c42a
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-10-18 19:12:58 +0400

    "Sunk time into scope creep of adding an autoSort(), additionally refactored code. Refactored code = W, autoSort = L. autoSort is supposed to make the list go back and forth."

--- linked-list.js
+++ linked-list.js
@@ -1,31 +1,42 @@
-console.log("heehee");
-
-/*It's not part of the project, but it's nice to have for modularity*/
-class Head {
-  constructor(next, value) {
-    this.next = next;
-    this.value = value;
-  }
-}
-
 class LinkedList {
   constructor() {
-    this.headObj = new Head(null, "HEAD");
+    this.next = null;
+    this.value = "HEAD";
     this.size = 0;
+  }
+
+  //we need a sort method to make it possible to go back
+  autoSort() {
+    if (this.size === 0 || this.next === null) {
+      return;
+    }
+
+    function sort(previousEl, nextEl, depth) {
+      if (depth === 0) {
+        return;
+      }
+
+      nextEl.previous = previousEl;
+      previousEl = nextEl;
+      nextEl = nextEl.next;
+      sort(previousEl, nextEl, depth - 1);
+    }
+
+    sort(this, this.next, this.size);
   }
 
   append(value) {
     const lastObj = this.tail();
     lastObj.next = new Node(value);
     this.size += 1;
+    this.autoSort();
   }
 
   prepend(value) {
-    // Make new object point to the one following the head
-    //Make header point to new object
     const newObj = new Node(value);
-    newObj.next = this.headObj.next;
-    this.headObj.next = newObj;
+    newObj.next = this.next;
+    this.next = newObj;
+    this.autoSort();
   }
 
   size() {
@@ -33,8 +44,8 @@ class LinkedList {
   }
 
   head() {
-    console.log(this.headObj.next);
-    return this.headObj.next;
+    console.log(this.next);
+    return this;
   }
 
   tail() {
@@ -46,7 +57,7 @@ class LinkedList {
         this.findTail(startPoint.next);
       }
     }
-    return findTail(this.headObj);
+    return findTail(this);
   }
 
   at(index) {
@@ -60,9 +71,13 @@ class LinkedList {
       }
     }
 
-    let elAtIndex = getToIndex(index, this.headObj);
+    let elAtIndex = getToIndex(index, this);
     console.log(elAtIndex);
     return elAtIndex;
+  }
+
+  pop() {
+    let lastObj = tail();
   }
 }
 
@@ -70,6 +85,7 @@ class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.previous = null;
   }
 }
 
